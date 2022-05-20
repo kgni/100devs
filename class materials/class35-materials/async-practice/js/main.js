f9dde8c870523d186af8b4aee32bf4280287eec9
kgni <admin@mdia.dk>
2022-05-20 10:41:48 +0200

    add try catch block to async function

--- class materials/class35-materials/async-practice/js/main.js
+++ class materials/class35-materials/async-practice/js/main.js
@@ -135,10 +135,13 @@
 // getPaid()
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
-
+async function getACuteDogPhoto() {
+	try {
+		const res = await fetch('https://dog.ceo/api/breeds/image/random');
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(`ERROR WHEN FETCHING ${error}`);
+	}
+}
+getACuteDogPhoto();
