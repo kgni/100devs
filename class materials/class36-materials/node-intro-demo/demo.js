654b95ac595bf47c04509f580260fcc78e5183d0
kgni <admin@mdia.dk>
2022-05-26 21:03:48 +0200

    add homework

--- class materials/class36-materials/node-intro-demo/demo.js
+++ class materials/class36-materials/node-intro-demo/demo.js
@@ -1,9 +1,20 @@
-const http = require('http')
-const fs = require('fs')
-http.createServer((req, res) => {
-  fs.readFile('demofile.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
-  })
-}).listen(8000)
+// const http = require('http');
+// const fs = require('fs');
+// http
+// 	.createServer((req, res) => {
+// 		fs.readFile('demofile.html', (err, data) => {
+// 			res.writeHead(200, { 'Content-Type': 'text/html' });
+// 			res.write(data);
+// 			res.end();
+// 		});
+// 	})
+// 	.listen(8000);
+
+function coinFlip() {
+	const randNum = Math.random();
+
+	console.log(randNum);
+	randNum >= 0.5 ? console.log('head') : console.log('tails');
+}
+
+coinFlip();
