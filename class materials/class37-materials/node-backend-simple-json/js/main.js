270e89ddd1fb1bbdf8f4019fd8a2b44e5f6ce395
kgni <admin@mdia.dk>
2022-05-29 21:40:44 +0200

    add rock paper scissors homework

--- class materials/class37-materials/node-backend-simple-json/js/main.js
+++ class materials/class37-materials/node-backend-simple-json/js/main.js
@@ -1,13 +1,34 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+document.querySelector('#clickMe').addEventListener('click', makeReq);
 
-async function makeReq(){
+async function makeReq() {
+	const choice = document.querySelector('#choice').value;
+	console.log(choice);
+	const res = await fetch(`/api?choice=${choice}`);
+	const data = await res.json();
 
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
-  const data = await res.json()
+	const playerData = data[0].player;
+	const computerData = data[1].computer;
 
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+	if (playerData.playerChoice === computerData.computerChoice) {
+		document.querySelector('.winner').innerText = `DRAW`;
+	} else {
+		const rules = {
+			rock: 'scissors',
+			paper: 'rock',
+			scissors: 'paper',
+		};
+		if (rules[playerData.playerChoice] === computerData.computerChoice) {
+			document.querySelector('.winner').innerText = `Player WON`;
+		} else {
+			document.querySelector('.winner').innerText = `Computer WON`;
+		}
+	}
+
+	console.log(data[0].player.playerChoice);
+	document.querySelector('.playerChoice').textContent = playerData.playerChoice;
+	document.querySelector('.playerImg').src = playerData.playerImg;
+
+	document.querySelector('.computerChoice').textContent =
+		computerData.computerChoice;
+	document.querySelector('.computerImg').src = computerData.computerImg;
 }
--- class materials/class37-materials/node-backend-simple-json/server.js
+++ class materials/class37-materials/node-backend-simple-json/server.js
@@ -1,78 +1,113 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
-const figlet = require('figlet')
+const figlet = require('figlet');
 
 const server = http.createServer((req, res) => {
-  const page = url.parse(req.url).pathname;
-  const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
-      }
-      res.write(data);
-      res.end();
-    });
-  }
+	const page = url.parse(req.url).pathname;
+	const params = querystring.parse(url.parse(req.url).query);
+	console.log(page);
+	if (page == '/') {
+		fs.readFile('index.html', function (err, data) {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(data);
+			res.end();
+		});
+	} else if (page == '/api') {
+		if ('choice' in params) {
+			const computerChoices = [
+				{
+					computerChoice: 'rock',
+					computerImg:
+						'https://i.pinimg.com/originals/4d/40/db/4d40dbef4879e329e9e9309ca3320470.png',
+				},
+				{
+					computerChoice: 'paper',
+					computerImg:
+						'https://www.nicepng.com/png/detail/45-455986_the-gallery-for-notebook-paper-texture-png-notebook.png',
+				},
+				{
+					computerChoice: 'scissors',
+					computerImg:
+						'https://upload.wikimedia.org/wikipedia/commons/0/0c/Small_pair_of_blue_scissors.png',
+				},
+			];
+			const randNum = Math.floor(Math.random() * computerChoices.length);
+
+			if (params['choice'] === 'rock') {
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				const objToJson = [
+					{
+						player: {
+							playerChoice: 'rock',
+							playerImg:
+								'https://i.pinimg.com/originals/4d/40/db/4d40dbef4879e329e9e9309ca3320470.png',
+						},
+					},
+
+					{
+						computer: computerChoices[randNum],
+					},
+				];
+				res.end(JSON.stringify(objToJson));
+			} //choice = leon
+			else if (params['choice'] === 'paper') {
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				const objToJson = [
+					{
+						player: {
+							playerChoice: 'paper',
+							playerImg:
+								'https://www.nicepng.com/png/detail/45-455986_the-gallery-for-notebook-paper-texture-png-notebook.png',
+						},
+					},
+
+					{
+						computer: computerChoices[randNum],
+					},
+				];
+				res.end(JSON.stringify(objToJson));
+			} else if (params['choice'] === 'scissors') {
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				const objToJson = [
+					{
+						player: {
+							playerChoice: 'scissors',
+							playerImg:
+								'https://upload.wikimedia.org/wikipedia/commons/0/0c/Small_pair_of_blue_scissors.png',
+						},
+					},
+
+					{
+						computer: computerChoices[randNum],
+					},
+				];
+				res.end(JSON.stringify(objToJson));
+			}
+		}
+	} else if (page == '/css/style.css') {
+		fs.readFile('css/style.css', function (err, data) {
+			res.write(data);
+			res.end();
+		});
+	} else if (page == '/js/main.js') {
+		fs.readFile('js/main.js', function (err, data) {
+			res.writeHead(200, { 'Content-Type': 'text/javascript' });
+			res.write(data);
+			res.end();
+		});
+	} else {
+		figlet('404!!', function (err, data) {
+			if (err) {
+				console.log('Something went wrong...');
+				console.dir(err);
+				return;
+			}
+			res.write(data);
+			res.end();
+		});
+	}
 });
 
 server.listen(8000);
