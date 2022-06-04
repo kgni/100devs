ba34b12616682239f75e6a2cbea913490c3497a1
mdiadk <acc@mdia.dk>
2022-06-04 22:07:31 +0200

    made some exercises

--- homework/week19/exercises/server.js
+++ homework/week19/exercises/server.js
@@ -3,3 +3,62 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+let persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+];
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = req.params.id;
+
+	const person = persons.find((el) => el.id == id);
+
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
+// delete request route. Here we are taking the id from the parameter, then we are reassigning the array to a new array containing every element, except the element that matches the id
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = req.params.id;
+
+	persons = persons.filter((person) => person.id != id);
+	console.log(persons);
+	res.json(persons);
+
+	// res.status(204).end();
+});
+
+app.get('/info', (req, res) => {
+	res.send(`Phonebook has info for ${persons.length} people\n ${new Date()}`);
+});
+
+app.listen(PORT, () => {
+	console.log(`LISTENING ON PORT ${PORT}`);
+});
